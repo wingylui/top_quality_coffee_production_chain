008b1c4358ffdc87fed60a74665765dc2ab8f4fc
wingylui <63942638+wingylui@users.noreply.github.com>
2023-03-21 16:10:31 +0800

    create database and obtain data from database

--- PostgreSQL/obtain_required_data.sql
+++ PostgreSQL/obtain_required_data.sql
@@ -2,12 +2,12 @@
 SELECT species_name FROM SPECIES_INFO LIMIT 100;
 
 -- agtron (whole bean),roast level and coffee price
-SELECT COFFEE.coffee_name, COFFEE.agtron_WB, R.roast_level, COFFEE.price_per_kg_USD
+SELECT COFFEE.coffee_ID, COFFEE.coffee_name, COFFEE.agtron_WB, R.roast_level, COFFEE.price_per_kg_USD
 FROM COFFEE_MAIN AS COFFEE
 INNER JOIN ROAST_LEVEL AS R ON R.roast_lv_id = COFFEE.roast_lv_id
 
 -- identifying the species diversity in different country
-SELECT  COFFEE.coffee_name, S.species_name, COUNTRY.country_name AS origin_country_name
+SELECT COFFEE.coffee_ID, COFFEE.coffee_name, S.species_name, COUNTRY.country_name AS origin_country_name
 FROM SPECIES_INFO AS S
 INNER JOIN SPECIES_LINK ON SPECIES_LINK.species_id = S.species_id
 INNER JOIN COFFEE_MAIN AS COFFEE ON COFFEE.coffee_id = SPECIES_LINK.coffee_id
@@ -15,7 +15,7 @@ INNER JOIN ORIGIN AS O ON O.coffee_id = COFFEE.coffee_id
 INNER JOIN COUNTRY ON COUNTRY.country_id = O.coffee_origin_country_id;
 
 -- coffee from roaster and their location
-SELECT COFFEE.coffee_name, R.raoster_name AS roaster_name, CITY.roaster_city,
+SELECT COFFEE.coffee_ID, COFFEE.coffee_name, R.raoster_name AS roaster_name, CITY.roaster_city,
 	COUNTRY.country_name AS roaster_country_name
 FROM SPECIES_INFO AS S
 INNER JOIN SPECIES_LINK ON SPECIES_LINK.species_id = S.species_id
